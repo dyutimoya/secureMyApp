e6623b3c0b2725e3a5c2cc4f18f766d9576a1bc8
DYUTIMOY <dyutimoya@gmail.com>
2024-06-29 17:27:38 +0530

    first commet

---
+++ secureMyAppLibrary/src/main/java/com/dyuti/securemyapplibrary/FirebaseVolleyHelper.java
@@ -0,0 +1,19 @@
+package com.dyuti.securemyapplibrary;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+public class FirebaseVolleyHelper {
+    private static final String BASE_URL = "https://projectcontroller-7b37a-default-rtdb.firebaseio.com/";
+    private static final String JSON_SUFFIX = ".json";
+    private final RequestQueue requestQueue;
+    public FirebaseVolleyHelper(RequestQueue requestQueue) {
+        this.requestQueue = requestQueue;
+    }
+    public void getData(String path, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        String url = BASE_URL + path + JSON_SUFFIX;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, listener, errorListener);
+        requestQueue.add(stringRequest);
+    }
+}
---
+++ secureMyAppLibrary/src/main/java/com/dyuti/securemyapplibrary/SecureNow.java
@@ -0,0 +1,22 @@
+package com.dyuti.securemyapplibrary;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class SecureNow {
+    public static void secure(Activity activity, String s) {
+        RequestQueue requestQueue = Volley.newRequestQueue(activity);
+        FirebaseVolleyHelper firebaseVolleyHelper = new FirebaseVolleyHelper(requestQueue);
+        firebaseVolleyHelper.getData(s, response -> {
+            if (response.equalsIgnoreCase("true")){
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setTitle("Alert");
+                builder.setMessage("You can't use this app. Contact your developer !");
+                builder.setCancelable(false);
+                builder.create().show();
+            }
+        }, error -> {});
+    }
+}
